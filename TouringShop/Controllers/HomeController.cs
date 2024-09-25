52b79c04fbf4533846086c0ce844f51bf7f71d4f
qanhpeo17 <154793506+qanhpeo17@users.noreply.github.com>
2024-09-25 15:52:05 +0700

    test

---
+++ TouringShop/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TouringShop.Models;
+namespace TouringShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        ChoBaoHo1Entities db = new ChoBaoHo1Entities();
+        public ActionResult Index()
+        {
+            List<SanPham> sp = db.SanPhams.ToList();
+            return View(sp);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
